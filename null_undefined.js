67cf210090f5f97d5ef0b2839cacbdf1ed060ce5
Dain Choi <twa05157@naver.com>
2020-10-15 02:45:36 +0900

    all example upload

---
+++ null_undefined.js
@@ -0,0 +1,10 @@
+//null, undefined
+let noting = null;
+console.log(`nothing : ${noting}, type: ${typeof noting}`);
+let x;
+console.log(`x: ${x}, type ${typeof x}`); //undefined
+
+//null vs undefined
+console.log('null vs undefined');
+console.log('null === undefined: ', null === undefined);
+console.log('null == undefined: ', null == undefined);
